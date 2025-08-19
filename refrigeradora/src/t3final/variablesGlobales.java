6f5e1500c67551083986f7f866c05db0de02ac19
haeerimhae <svndae.j.one@gmail.com>
2025-08-18 21:38:31 -0500

    commit prueba

---
+++ refrigeradora/src/t3final/variablesGlobales.java
@@ -0,0 +1,7 @@
+package t3final;
+
+public class variablesGlobales {
+	public static String nombreUsuario = "Invitado";
+    public static int contadorClicks = 0;
+    public static boolean modoOscuro = false;
+}
