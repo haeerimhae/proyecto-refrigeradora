d0d824cd89334e002c368efcdc16d63023bab7c1
haeerimhae <svndae.j.one@gmail.com>
2025-08-21 11:28:26 -0500

    uniendo proyecto

--- refrigeradora/src/t3final/ConsultarRefrigeradora.java
+++ refrigeradora/src/t3final/ConsultarRefrigeradora.java
@@ -1,168 +1,122 @@
 package t3final;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.EventQueue;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
+import java.awt.Color;
 import javax.swing.DefaultComboBoxModel;
-import javax.swing.JTextField;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeEvent;
-import javax.swing.event.AncestorListener;
-import javax.swing.event.AncestorEvent;
 
-public class ConsultarRefrigeradora extends JDialog {
+public class ConsultarRefrigeradora extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private final JPanel contentPanel = new JPanel();
-	private JTextField txtPrecio;
-	private JTextField txtAncho;
-	private JTextField txtAlto;
-	private JTextField txtFondo;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_3;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		try {
-			ConsultarRefrigeradora dialog = new ConsultarRefrigeradora();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ConsultarRefrigeradora frame = new ConsultarRefrigeradora();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	/**
-	 * Create the dialog.
+	 * Create the frame.
 	 */
 	public ConsultarRefrigeradora() {
-		setTitle("Consultar  refrigeradora");
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.addAncestorListener(new AncestorListener() {
-			public void ancestorAdded(AncestorEvent event) {
-				System.out.print("LoadForm");
-				txtPrecio.setText("" + Proyecto.precio0);
-				txtFondo.setText("" + Proyecto.fondo0);
-				txtAncho.setText("" + Proyecto.ancho0);
-				txtAlto.setText("" + Proyecto.alto0);
-			}
-
-			public void ancestorMoved(AncestorEvent event) {
-			}
-
-			public void ancestorRemoved(AncestorEvent event) {
-			}
-		});
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
-
-		JLabel lblModelo = new JLabel("Modelo");
-		lblModelo.setBounds(10, 22, 75, 14);
-		contentPanel.add(lblModelo);
-
-		JLabel lblPrecio = new JLabel("Precio (S/)");
-		lblPrecio.setBounds(10, 62, 75, 14);
-		contentPanel.add(lblPrecio);
-
-		JLabel lblAncho = new JLabel("Ancho (cm)");
-		lblAncho.setBounds(10, 104, 75, 14);
-		contentPanel.add(lblAncho);
-
-		JLabel lblAlto = new JLabel("Alto (cm)");
-		lblAlto.setBounds(10, 150, 75, 14);
-		contentPanel.add(lblAlto);
-
-		JLabel lblFondo = new JLabel("Fondo (cm)");
-		lblFondo.setBounds(10, 198, 85, 14);
-		contentPanel.add(lblFondo);
-
-		JComboBox cboModelo = new JComboBox();
-		cboModelo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				int pos = cboModelo.getSelectedIndex();
-
-				switch (pos) {
-				case 0:
-					txtPrecio.setText(Proyecto.precio0 + "");
-					txtAncho.setText(Proyecto.ancho0 + "");
-					txtAlto.setText(Proyecto.alto0 + "");
-					txtFondo.setText(Proyecto.fondo0 + "");
-					break;
-				case 1:
-					txtPrecio.setText(Proyecto.precio1 + "");
-					txtAncho.setText(Proyecto.ancho1 + "");
-					txtAlto.setText(Proyecto.alto1 + "");
-					txtFondo.setText(Proyecto.fondo1 + "");
-					break;
-				case 2:
-					txtPrecio.setText(Proyecto.precio2 + "");
-					txtAncho.setText(Proyecto.ancho2 + "");
-					txtAlto.setText(Proyecto.alto2 + "");
-					txtFondo.setText(Proyecto.fondo2 + "");
-					break;
-				case 3:
-					txtPrecio.setText(Proyecto.precio3 + "");
-					txtAncho.setText(Proyecto.ancho3 + "");
-					txtAlto.setText(Proyecto.alto3 + "");
-					txtFondo.setText(Proyecto.fondo3 + "");
-					break;
-				case 4:
-					txtPrecio.setText(Proyecto.precio4 + "");
-					txtAncho.setText(Proyecto.ancho4 + "");
-					txtAlto.setText(Proyecto.alto4 + "");
-					txtFondo.setText(Proyecto.fondo4 + "");
-				}
-
-			}
-		});
-
-		cboModelo.setBounds(120, 18, 286, 22);
-		cboModelo.setModel(new DefaultComboBoxModel(new String[] { Proyecto.modelo0, Proyecto.modelo1, Proyecto.modelo2,
-				Proyecto.modelo3, Proyecto.modelo4 }));
-
-		contentPanel.add(cboModelo);
-
-		txtPrecio = new JTextField();
-		txtPrecio.setBounds(120, 59, 140, 20);
-		contentPanel.add(txtPrecio);
-		txtPrecio.setColumns(10);
-		txtPrecio.setEditable(false);
-
-		txtAncho = new JTextField();
-		txtAncho.setBounds(120, 101, 140, 20);
-		contentPanel.add(txtAncho);
-		txtAncho.setColumns(10);
-		txtAncho.setEditable(false);
-
-		txtAlto = new JTextField();
-		txtAlto.setBounds(120, 147, 140, 20);
-		contentPanel.add(txtAlto);
-		txtAlto.setColumns(10);
-		txtAlto.setEditable(false);
-
-		txtFondo = new JTextField();
-		txtFondo.setBounds(120, 195, 140, 20);
-		contentPanel.add(txtFondo);
-		txtFondo.setColumns(10);
-		txtFondo.setEditable(false);
-
-		JButton btnCerrar = new JButton("Cerrar");
-		btnCerrar.setBounds(317, 58, 89, 146);
+		setBackground(new Color(255, 239, 213));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 360, 202);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(253, 245, 230));
+		contentPane.setForeground(new Color(250, 235, 215));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnCerrar = new JButton("Regresar ");
 		btnCerrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				dispose();
 			}
 		});
-		contentPanel.add(btnCerrar);
+		btnCerrar.setFont(new Font("Bookman Old Style", Font.PLAIN, 11));
+		btnCerrar.setBackground(new Color(250, 235, 215));
+		btnCerrar.setForeground(new Color(0, 0, 0));
+		btnCerrar.setBounds(220, 29, 99, 36);
+		contentPane.add(btnCerrar);
+		
+		JLabel cboModelo = new JLabel("Modelo");
+		cboModelo.setFont(new Font("Bookman Old Style", Font.PLAIN, 11));
+		cboModelo.setBounds(10, 29, 46, 14);
+		contentPane.add(cboModelo);
+		
+		JLabel cboPrecio = new JLabel("Precio");
+		cboPrecio.setFont(new Font("Bookman Old Style", Font.PLAIN, 11));
+		cboPrecio.setBounds(10, 57, 46, 14);
+		contentPane.add(cboPrecio);
+		
+		JLabel cboAncho = new JLabel("Ancho");
+		cboAncho.setFont(new Font("Bookman Old Style", Font.PLAIN, 11));
+		cboAncho.setBounds(10, 82, 46, 14);
+		contentPane.add(cboAncho);
+		
+		JLabel cboAlto = new JLabel("Alto");
+		cboAlto.setFont(new Font("Bookman Old Style", Font.PLAIN, 11));
+		cboAlto.setBounds(10, 107, 46, 14);
+		contentPane.add(cboAlto);
+		
+		JLabel cboFondr = new JLabel("Fondo");
+		cboFondr.setFont(new Font("Bookman Old Style", Font.ITALIC, 11));
+		cboFondr.setBounds(10, 132, 46, 14);
+		contentPane.add(cboFondr);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Calibri Light", Font.PLAIN, 11));
+		comboBox.setBackground(new Color(250, 235, 215));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"LG LTCS20020W", " Samsung RB195ACRS", " Whirlpool WRT318FZDM ", "Bosch B36BT930NS", "LG LSXS26366S"}));
+		comboBox.setForeground(new Color(0, 0, 0));
+		comboBox.setBounds(55, 21, 106, 22);
+		contentPane.add(comboBox);
+		
+		textField = new JTextField();
+		textField.setBounds(55, 54, 106, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(55, 82, 106, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(55, 132, 106, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(55, 107, 106, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+
 	}
 }
